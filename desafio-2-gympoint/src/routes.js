62fd3232d739501b51f4fe5a6b08c50d711464b3
Joao Santana <joaosantana.ti@gmail.com>
2019-10-22 01:14:27 -0300

    Creating students by admin with jwt

--- desafio-2-gympoint/src/routes.js
+++ desafio-2-gympoint/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import SigninController from './app/controllers/SigninController';
+import StudentController from './app/controllers/StudentsController';
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.get('/health_check', (req, res) => res.json({ message: 'OK Gympoint' }));
 
 routes.post('/signin', SigninController.store);
+
+routes.use(auth);
+routes.post('/students', StudentController.store);
+// routes.get('/students', S)
 
 export default routes;
