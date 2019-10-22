8e3c319448aec2e71bd03280761b49c8ea7d1605
Joao Santana <joaosantana.ti@gmail.com>
2019-10-22 00:38:32 -0300

    Creating signin endpoint

--- desafio-2-gympoint/src/routes.js
+++ desafio-2-gympoint/src/routes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
+import SigninController from './app/controllers/SigninController';
 
 const routes = new Router();
 
 routes.get('/health_check', (req, res) => res.json({ message: 'OK Gympoint' }));
+
+routes.post('/signin', SigninController.store);
 
 export default routes;
