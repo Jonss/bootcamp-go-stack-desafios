234e7bd21b2dc3b437bd5b454d4cae77d4e9f856
Joao Santana <joaosantana.ti@gmail.com>
2019-10-28 20:48:56 -0300

    Create enrollment

---
+++ desafio-3-gympoint/src/app/controllers/EnrollmentController.js
@@ -0,0 +1,59 @@
+import { addMonths, parseISO } from 'date-fns';
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+import Enrollment from '../models/Enrollment';
+
+class EnrollmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      const errors = await schema.validate(req.body).catch(err => err.errors);
+      return res.status(400).json(errors);
+    }
+
+    const { plan_id, student_id, start_date } = req.body;
+
+    if (await Enrollment.findOne({ where: { student_id } })) {
+      return res.status(400).json({ error: 'user already in a plan' });
+    }
+
+    const plan = await Plan.findByPk(plan_id);
+
+    if (!plan) {
+      return res.status(400).json({ error: `Plan ${plan_id} not found` });
+    }
+
+    const end_date = addMonths(parseISO(start_date), plan.duration);
+    const plan_title = plan.title;
+    const price = plan.duration * plan.price;
+
+    const { id } = await Enrollment.create({
+      start_date,
+      end_date,
+      plan_id,
+      student_id,
+      price,
+    });
+
+    return res.json({
+      id,
+      end_date,
+      start_date,
+      plan: {
+        title: plan.title,
+        duration: plan.duration,
+        price: plan.price,
+      },
+      price,
+      student_id,
+      plan_title,
+    });
+  }
+}
+
+export default new EnrollmentController();
--- desafio-3-gympoint/src/routes.js
+++ desafio-3-gympoint/src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import SigninController from './app/controllers/SigninController';
 import StudentController from './app/controllers/StudentsController';
 import PlanController from './app/controllers/PlanController';
+import EnrollmentController from './app/controllers/EnrollmentController';
 
 import auth from './app/middlewares/auth';
 
@@ -20,5 +21,7 @@ routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.delete('/plans/:id', PlanController.delete);
 routes.patch('/plans/:id', PlanController.update);
+
+routes.post('/enrollments', EnrollmentController.store);
 
 export default routes;
